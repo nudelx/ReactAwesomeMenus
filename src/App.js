09d5e3f19f0b3c33f0a0b6f792a0cbd3048710aa
nudelx <alex.nudelman@samanage.com>
2018-07-10 18:29:48 +0300

    new design and css fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css'
+
 import React, { Component } from 'react'
+
 import Header from './componets/header'
 import Stage from './componets/stage'
+import TitleCard from './componets/titleCard'
+import Welcome from './componets/welcome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <TitleCard>
+          <Welcome />
+        </TitleCard>
         <Stage />
       </div>
     )
